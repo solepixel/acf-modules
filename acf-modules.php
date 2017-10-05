583d74df6a7716ef05efe40c51f970e2b1c16404
Brian DiChiara <me@briandichiara.com>
2017-10-05 12:28:54 -0500

    updated version to 1.1.0
    changed before_action to be option, removed default genesis support

--- acf-modules.php
+++ acf-modules.php
@@ -1,18 +1,19 @@
 <?php
-/*
-Plugin Name: ACF Modules
-Plugin URI: http://www.briandichiara.com/
-Description: Customizable content modules for modular content.
-Version: 1.0.2
-Author: Brian DiChiara
-Author URI: http://www.briandichiara.com/
-Text Domain: acfmod
-License: GNU GPL v2.0
-*/
+/**
+ * Plugin Name: ACF Modules
+ * Plugin URI: http://www.briandichiara.com/
+ * Description: Customizable content modules for modular content.
+ * Version: 1.1.0
+ * Author: Brian DiChiara
+ * Author URI: https://www.briandichiara.com/
+ * Text Domain: acfmod
+ * License: GNU GPL v2.0
+ *
+ * @package acf-modules
+ */
 
 /*
-
-# Use this snippet to add new modules
+// Use this snippet to add new modules.
 
 add_filter( 'acfmod/layouts', 'acfmod_layout_[module]', 70 );
 
@@ -28,18 +29,17 @@ function acfmod_modules_[module](){
 	$output = get_sub_field( [module] );
 	return $output;
 }
-
 */
 
-define( 'ACFMOD_VERSION', '1.0.0' );
+define( 'ACFMOD_VERSION', '1.1.0' );
 define( 'ACFMOD_PATH', plugin_dir_path( __FILE__ ) );
 define( 'ACFMOD_URI', plugin_dir_url( __FILE__ ) );
 
 /* Load up any helper functions */
-include_once( 'includes/helpers.php' );
+include_once( ACFMOD_PATH . 'includes/helpers.php' );
 
 /* Create the Modules */
-include_once( 'includes/init.php' );
+include_once( ACFMOD_PATH . 'includes/init.php' );
 
 /**
  * These hooks/functions do all the magic
@@ -47,43 +47,63 @@ include_once( 'includes/init.php' );
 
 add_filter( 'the_content', 'acfmod_insert_modules' );
 
-function acfmod_insert_modules( $content = '' ){
-	if( ! did_action( apply_filters( 'acsmod/after_action', 'genesis_before_loop' ) ) )
-		return $content;
+/**
+ * Insert Modules into the_content
+ *
+ * @param string $content  The WP Content field.
+ * @return string          HTML including Modules content
+ */
+function acfmod_insert_modules( $content = '' ) {
+	// Use genesis_before_loop or something else to prevent filtering the_content too soon.
+	$after_action = apply_filters( 'acsmod/after_action', '' );
 
-	if( ! function_exists( 'have_rows' ) )
+	if ( ! empty( $after_action ) && ! did_action( $after_action ) ) {
 		return $content;
+	}
 
-	if( have_rows( '_acfmod_modules' ) ):
+	if ( ! function_exists( 'have_rows' ) ) {
+		return $content;
+	}
+
+	if ( have_rows( '_acfmod_modules' ) ) {
 		global $acfmod_sections, $acfmod_section_open;
 		$acfmod_sections = 0;
 		$acfmod_section_open = false;
 
-		# truncate anything in the content field
+		// Truncate anything in the content field.
 		$original_content = $content; // probably unnecessary, but what the heck.
 		$content = '';
 
-		while ( have_rows( '_acfmod_modules' ) ) : the_row();
+		while ( have_rows( '_acfmod_modules' ) ) :
+			the_row();
 			$content .= acfmod_module_loop( 'content' );
 		endwhile;
 
 		$content .= acfmod_close_sections();
 
-	endif;
+	}
 
 	return $content;
 }
 
 global $acfmod_sections, $acfmod_current_section, $acfmod_module_count;
 
-function acfmod_module_loop( $context = NULL ){
+/**
+ * Loop through all the modules and return the module HTML
+ *
+ * @param string $context  Context of the current element.
+ * @return string          Module HTML
+ */
+function acfmod_module_loop( $context = null ) {
 	global $acfmod_sections, $acfmod_current_section, $acfmod_module_count;
 
-	if( ! is_array( $acfmod_sections ) )
+	if ( ! is_array( $acfmod_sections ) ) {
 		$acfmod_sections = array();
+	}
 
-	if( ! $acfmod_module_count )
+	if ( empty( $acfmod_module_count ) ) {
 		$acfmod_module_count = 0;
+	}
 
 	$content = '';
 	$style = '';
@@ -91,54 +111,65 @@ function acfmod_module_loop( $context = NULL ){
 	$module = apply_filters( 'acfmod/modules/' . get_row_layout(), '' );
 	$styles = get_sub_field( 'module_styles' );
 
-	if( get_row_layout() == 'section' ){
-		if( get_sub_field( 'close_previous_section' ) ){
+	if ( 'section' === get_row_layout() ) {
+		if ( get_sub_field( 'close_previous_section' ) ) {
 			$content .= acfmod_close_section();
 		}
 
 		$content .= acfmod_open_section();
 	}
 
-	if( $module ){
+	if ( $module ) {
 		$acfmod_module_count++;
 		$col_width = '';
 		$section_open = false;
 
-		if( isset( $acfmod_sections[ $acfmod_current_section ] ) ){
+		if ( isset( $acfmod_sections[ $acfmod_current_section ] ) ) {
 			$acfmod_sections[ $acfmod_current_section ]->current_col++;
 			$col_width = $acfmod_sections[ $acfmod_current_section ]->get_col_width();
 			$section_open = $acfmod_sections[ $acfmod_current_section ]->is_open();
 		}
 
-		if( $col_width || $styles ):
+		if ( ! empty( $col_width ) || ! empty( $styles ) ) {
 			$content .= '<style type="text/css">';
-				$content .= '.module.module-' . $acfmod_module_count . ',.section.two-column>.module.module-' . $acfmod_module_count . ',.section.three-column>.module.module-' . $acfmod_module_count . ',.section.four-column>.module.module-' . $acfmod_module_count . ' {';
-					if( $col_width ):
-						$content .= 'width: ' . $col_width . '%;';
-					endif;
-					if( $styles ):
-						$content .= $styles;
-					endif;
-				$content .= '}';
+
+			$content .= '.module.module-' . absint( $acfmod_module_count ) . ',.section.two-column>.module.module-' . absint( $acfmod_module_count ) . ',.section.three-column>.module.module-' . absint( $acfmod_module_count ) . ',.section.four-column>.module.module-' . absint( $acfmod_module_count ) . ' {';
+
+			if ( $col_width ) {
+				$content .= 'width: ' . esc_html( $col_width ) . '%;';
+			}
+
+			if ( $styles ) {
+				$content .= $styles;
+			}
+
+			$content .= '}';
+
 			$content .= '</style>';
-		endif;
+		}
 
-		$content .= '<div class="module module-' . get_row_layout() . ' module-' . $acfmod_module_count . '">';
-			if( ! $section_open )
-				if( function_exists( 'genesis_structural_wrap' ) )
-					$content .= genesis_structural_wrap( 'modular-content', 'open', false );
+		$content .= '<div class="module module-' . esc_attr( get_row_layout() ) . ' module-' . esc_attr( $acfmod_module_count ) . '">';
 
-				$content .= '<div class="module-inner">';
-					$content .= $module;
-				$content .= '</div><!-- .module-inner -->';
+		if ( ! $section_open ) {
+			if ( function_exists( 'genesis_structural_wrap' ) ) {
+				$content .= genesis_structural_wrap( 'modular-content', 'open', false );
+			}
+		}
 
-			if( ! $section_open )
-				if( function_exists( 'genesis_structural_wrap' ) )
-					$content .= genesis_structural_wrap( 'modular-content', 'close', false );
+		$content .= '<div class="module-inner">';
+			$content .= $module;
+		$content .= '</div><!-- .module-inner -->';
+
+		if ( ! $section_open ) {
+			if ( function_exists( 'genesis_structural_wrap' ) ) {
+				$content .= genesis_structural_wrap( 'modular-content', 'close', false );
+			}
+		}
+
 		$content .= '</div><!-- .module -->';
 	}
 
-	if( get_row_layout() == 'section_closer' ){
+	if ( 'section_closer' === get_row_layout() ) {
 		$content .= acfmod_close_section();
 	}
 
@@ -149,10 +180,16 @@ function acfmod_module_loop( $context = NULL ){
 
 add_filter( 'body_class', 'acfmod_modular_body_class' );
 
-function acfmod_modular_body_class( $body_class ){
-	if( have_rows( '_acfmod_modules' ) && get_queried_object_id() ):
+/**
+ * Add body class for module enabled pages
+ *
+ * @param array $body_class  Body Classes array.
+ * @return array             Potentially modified array
+ */
+function acfmod_modular_body_class( $body_class ) {
+	if ( have_rows( '_acfmod_modules' ) && get_queried_object_id() ) {
 		$body_class[] = 'modular';
-	endif;
+	}
 
 	return $body_class;
 }
